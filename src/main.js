a46ac3b430f428556b2305d1a64abcc6c2fc38c9
gounygregoire <gouny.gregoire@gmail.com>
2025-07-31 11:48:47 +0200

    Add jsPDF dependency and update build

--- src/main.js
+++ src/main.js
@@ -3,7 +3,9 @@ import "../static/js/error-handler.js";
 import "../static/js/upload-handler.js";
 import "../static/js/design-insights.js";
 import { initViewer, loadXeokitSDK } from "./xeokit_viewer.js";
+import { jsPDF } from "jspdf";
 
 window.Viewer = Viewer;
 window.initViewer = initViewer;
 window.loadXeokitSDK = loadXeokitSDK;
+window.jsPDF = jsPDF;
