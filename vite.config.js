45e92997a7c43ee020c3a0d4b54431e5b234db80
gounygregoire <gouny.gregoire@gmail.com>
2025-09-01 09:07:35 +0000

    test

--- vite.config.js
+++ vite.config.js
@@ -12,6 +12,6 @@ export default defineConfig({
         chunkFileNames: '[name].js'
       }
     }
-  }
-base: '/'                  // simple et sûr à la racine
+  },
+base: '/',                  // simple et sûr à la racine
 })
