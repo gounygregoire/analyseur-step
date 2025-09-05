8df4d0b4aab531e8f1936f4526c2f60e827b8ecc
gounygregoire <gouny.gregoire@gmail.com>
2025-09-05 06:51:05 +0000

    test

--- celery_app.py
+++ celery_app.py
@@ -16,15 +16,10 @@ def make_celery():
     if backend and backend.startswith("rediss://"):
         app.conf.redis_backend_use_ssl = ssl_opts
 
-    # 🔎 Ajoute ces prints pour vérifier en logs côté Web Service :
-    print("[CELERY] broker:", broker)
-    print("[CELERY] backend:", backend)
-    print("[CELERY] broker_use_ssl:", app.conf.get("broker_use_ssl"))
-    print("[CELERY] redis_backend_use_ssl:", app.conf.get("redis_backend_use_ssl"))
-
     return app
 
 celery = make_celery()
+
 
 def init_celery(flask_app):
     celery.conf.update(flask_app.config)
